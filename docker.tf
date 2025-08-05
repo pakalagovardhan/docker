8d6a85ce38333a30360651abac25917fbacaeaab
pakalagovardhan <p.govardhan321@gmail.com>
2025-08-05 16:40:51 +0530

    add

--- docker.tf
+++ docker.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "docker" {
   ami           = local.ami_id
   #instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
-  instance_type = "t3.medium"
+  instance_type = "c7i-flex.large"
+  #instance_type = "t3.medium"
   # need more for terraform
   root_block_device {
     volume_size = 50
